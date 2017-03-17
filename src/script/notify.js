991bca4dba5392963bf38b8a3cc991f48884c27e
Duncan Werner <dwerner@riskamp.com>
2017-03-17 11:11:41 -0700

    add missing files

---
+++ src/script/notify.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2016-2017 Structured Data, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+"use strict";
+
+const { Utils } = require( "./util2.js" );
+
+const DEFAULT_TIMEOUT = 3; // seconds
+
+let panel;
+
+const createPanel = function(){
+
+  panel = document.createElement( "div" );
+  panel.className = "notifier-panel";
+  document.body.appendChild( panel );
+ 
+};
+
+module.exports.notify = function( opts ){
+
+  opts = opts || {};
+
+  if( !panel ) createPanel();
+
+  let node = document.createElement( "div" );
+  node.className = "notifier-message " + opts.className || "";
+
+  Utils.parseHTML( `<div class='notifier-icon'></div>
+    <div class='notifier-title'>${opts.title || ""}</div>
+    <div class='notifier-body'>${opts.body || ""}</div>
+    <div class='footer'>${opts.footer || ""}</div>`, node );
+
+  // default to top of stack, optionally on bottom
+
+  if( opts.bottom ) panel.appendChild( node );
+  else panel.insertBefore( node, panel.firstChild );
+
+  return new Promise( function( resolve, reject ){
+
+    let timer = null;
+    let clicked = false;
+    let clickEvent = null;
+    let expiring = false;
+
+    let click = function(e){
+      e.stopPropagation();
+      e.preventDefault();
+      clickEvent = e;
+      if( timer ) clearTimeout( timer );
+      clicked = true;
+      if( !expiring ){
+        node.removeEventListener( "transitionend", expire );
+        node.removeEventListener( "transitionend", cleanup );
+        node.addEventListener( "transitionend", cleanup );
+        setTimeout( function(){ node.style.opacity = 0; }, 1 );
+      }
+    };
+
+    let expire = function(){
+      node.removeEventListener( "transitionend", expire );
+      node.addEventListener( "transitionend", cleanup );
+      if( opts.timeout !== Infinity ){
+        let ms = ( opts.timeout || DEFAULT_TIMEOUT ) * 1000;
+        timer = setTimeout( function(){ expiring = true; node.style.opacity = 0; }, ms );
+      }
+    };
+
+    let cleanup = function(){
+      node.removeEventListener( "click", click );
+      node.removeEventListener( "transitionend", cleanup );
+      node.removeEventListener( "transitionend", expire ); // JIC
+      node.parentNode.removeChild( node );
+      resolve( { reason: clicked ? "click" : "timeout", event: clickEvent });
+    };
+
+    node.addEventListener( "click", click );
+    node.addEventListener( "transitionend", expire );
+
+    requestAnimationFrame(function(){ node.style.opacity=1; });
+
+  });
+
+};
+
---
+++ src/script/update-check.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2016-2017 Structured Data, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+"use strict";
+
+const https = require( "https" );
+
+let request = function(force){
+  return new Promise( function( resolve, reject ){
+    var options = {
+      host: 'api.github.com',
+      path: '/repos/sdllc/Basic-Excel-R-Toolkit/releases/latest',
+      headers: { 
+        'user-agent': 'https://github.com/sdllc/BERT-Console'
+      }
+    };
+
+    if( !force && settings.update.etag ) options.headers['If-None-Match'] = settings.update.etag;
+    let callback = function(response) {
+      let str = '';
+      response.on('data', function (chunk) { str += chunk; });
+      response.on('end', function () {
+        let data;
+        try {
+          data = str.length ? JSON.parse(str) : {};
+          resolve({ data: data, headers: response.headers, status: response.statusCode });
+        }
+        catch(e){ reject(e); }
+      });
+    };
+
+    https.request(options, callback).end();
+  });
+};
+
+module.exports.checkForUpdates = function( settings, force ){
+  if( !settings.update ){
+    settings.__broadcast__ = false;
+    settings.update = { etag: false, 'last-version': 0, 'notify-version': 0 };
+    settings.__broadcast__ = true;
+  }
+  return new Promise( function( resolve, reject ){
+    request( force ).then( function( rsp ){
+      if( rsp.status === 200 ){
+        if( rsp.headers.etag ) settings.update.etag = rsp.headers.etag;
+        if( rsp.data.tag_name ){
+          let version = String( rsp.data.tag_name.substr(1));
+          settings.update['last-version'] = version;
+        }
+      }
+      resolve();
+    });
+  });
+};
+
